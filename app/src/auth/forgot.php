3f31e15f3ba6d64ca19e6df7a3a8288a99211af0
blacruz <beitab90@gmail.com>
2015-05-31 14:43:12 +0200

    Req.3.3 Recuperar contrasenia

---
+++ app/src/auth/forgot.php
@@ -0,0 +1,59 @@
+<?php
+// Pear Mail Library
+require_once VENDOR_DIR.'/swiftmailer/autoload.php';
+
+if (isset($_POST) && count($_POST)>0 && !isset($_SESSION['PHPSESSID']) && !isset($_SESSION['USER_ID'])){
+    
+    require_once PROJECT_DIR.'/db_connection.php';
+    $query = "SELECT contrasenia, email FROM Usuario WHERE `Nombre_usuario`='".$_POST["user"]."' OR `email`='".$_POST["user"]."'";
+    $result = mysqli_query($conn,$query);
+    $conn->close();
+    if(!$result || $result->num_rows == 0){
+        $smarty->assign('noUserError','true');
+    } else {
+        while ($row=mysqli_fetch_assoc($result)){
+            $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+               ->setUsername('practicasoftware2@gmail.com')
+               ->setPassword('practicais2');
+
+            $mailer = Swift_Mailer::newInstance($transport);
+            $enlace = "<p>Dirijase a <a href='http://localhost:8888/tracker.php/login'>http://localhost:8888/tracker.php/login</a></p>";
+            $message = Swift_Message::newInstance('Recuperación contraseña')
+                ->setFrom(array('practicasoftware2@gmail.com' => 'Héctor Ambit'))
+                ->setTo(array($row['email']))
+                ->setBody("<p>Su contraseña es: ".$row['contrasenia']."</p>".$enlace, 'text/html');
+            try {
+                $mail = $mailer->send($message);
+                $smarty->assign('mailSended',$row['email']);
+            } catch (Swift_RfcComplianceException $e) {
+                $smarty->assign('failEmailError','true');
+            } catch (Swift_TransportException $e){
+                $smarty->assign('failError','true');
+            }
+                    
+        }
+    }
+    $smarty->display('forgot.tpl');
+
+// $result = $mailer->send($message);
+} else if (isset($_SESSION['PHPSESSID']) && isset($_SESSION['USER_ID'])){
+    header("Location: http://localhost:8888/tracker.php");
+    die();
+} else{
+    $smarty->display('forgot.tpl');
+}
+
+
+// $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+//   ->setUsername('h.ambit.h@gmail.com')
+//   ->setPassword('*zxc*JKL*qwe890?');
+
+// $mailer = Swift_Mailer::newInstance($transport);
+
+// $message = Swift_Message::newInstance('Test')
+//   ->setFrom(array('h.ambit.h@gmail.com' => 'Héctor Ambit'))
+//   ->setTo(array('beitab90@gmail.com'))
+//   ->setBody('Hola Cosita');
+
+
+// $result = $mailer->send($message);
---
+++ app/src/templates_c/6def1312a8bf2addb52f93b8f44953e8158ee654.file.forgot.tpl.php
@@ -0,0 +1,104 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-05-31 14:40:13
+         compiled from "auth/templates/forgot.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:1537513899556af76ea7ceb0-86516776%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '6def1312a8bf2addb52f93b8f44953e8158ee654' => 
+    array (
+      0 => 'auth/templates/forgot.tpl',
+      1 => 1433075694,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1537513899556af76ea7ceb0-86516776',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_556af76ed7e052_68631490',
+  'variables' => 
+  array (
+    'mailSended' => 0,
+    'failError' => 0,
+    'noUserError' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_556af76ed7e052_68631490')) {function content_556af76ed7e052_68631490($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+<head>
+	<title>SSTF - Grupo 5</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="ISW2-Grupo5">
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<!-- Custom styles -->
+	<style>
+		body {
+			padding-top: 40px;
+			padding-bottom: 40px;
+			background-color: white;
+			text-align: center;
+		}
+		.form-signin {
+			max-width: 330px;
+			padding: 15px;
+			padding-bottom: 5px;
+			margin: 0 auto;
+		}
+		.form-signin .form-signin-heading,
+		.form-signin .checkbox {
+			margin-bottom: 10px;
+		}
+		.form-signin .form-control:focus {
+			z-index: 2;
+		}
+		.form-signin input[type="text"] {
+			margin-bottom: 10px;
+			border-bottom-right-radius: 0;
+			border-bottom-left-radius: 0;
+		}
+		.form-signin input[type="password"] {
+			margin-bottom: 10px;
+			border-top-left-radius: 0;
+			border-top-right-radius: 0;
+		}
+		.btn-register{
+			width: 300px;
+			margin-bottom: 5px;
+		}
+	</style>
+</head>
+<body class=>
+	<div class="container">
+		<?php if (isset($_smarty_tpl->tpl_vars['mailSended']->value)) {?>
+			<h3 class="form-signin-heading">Correo de recuperación de contraseña mandado a la dirección:</h3>
+			<p><?php echo $_smarty_tpl->tpl_vars['mailSended']->value;?>
+</p>
+			<a href="login" class="btn btn-primary btn-register" type="submit">Iniciar sesión</a>
+		<?php } else { ?>
+		<?php if (isset($_smarty_tpl->tpl_vars['failError']->value)) {?>
+		<p class="text-danger">Se ha producido un error en el proceso. Intentelo de nuevo más tarde</p>
+		<?php }?>
+		<?php if (isset($_smarty_tpl->tpl_vars['failError']->value)) {?>
+		<p class="text-danger">La dirección de envio no es una dirección valida</p>
+		<?php }?>
+		<form class="form-signin" method='post' action=''>
+			<h3 class="form-signin-heading">Sistema Simplificado de Tracker Financiero</h3>
+			<hr>
+			<label for="inputUser" class="sr-only">Nobre de usuario o E-mail</label>
+			<input type="text" name="user" id="inputUser" class="form-control" placeholder="Nobre de usuario o E-mail" required>
+			<?php if (isset($_smarty_tpl->tpl_vars['noUserError']->value)) {?>
+				<p class="text-danger">El usuario o e-mail no existe</p>
+			<?php }?>
+			<button class="btn btn-primary btn-block" type="submit">Recuperar contraseña</button>
+		</form>
+		<?php }?>
+	</div>
+	<footer class="footer">
+	<p>&copy; Grupo05 - ISW2 2014-2015</p>
+	</footer>
+</body>
+</html><?php }} ?>
